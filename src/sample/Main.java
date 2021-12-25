6e053f313ba54fb5163bf98b327c3e5e7e53f582
aaghos97 <aaghos97@gmail.com>
2021-12-25 05:22:42 +0100

    First Structure

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        int y;
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
     }
 
 
