4d727ca21323a3954b7645e68088dba7758d35fe
aaghos97 <aaghos97@gmail.com>
2021-12-25 06:18:02 +0100

    Scenes and stages

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,40 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    final int window_width = 620;
+    final  int window_height = 620;
+    boolean fullscreen = false;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        int y;
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+        Image icon = new Image("/icon.png");
+        primaryStage.setTitle("Snake Game");
+        primaryStage.getIcons().add(icon);
+        primaryStage.setWidth(window_width);
+        primaryStage.setHeight(window_height);
+        primaryStage.setScene(scene);
+        primaryStage.setFullScreen(fullscreen);
         primaryStage.show();
 
+        Text start_text = new Text("Press Start!");
+        start_text.setX(50);
+        start_text.setY(50);
+        start_text.setFont(Font.font("Arial", 35));
+        start_text.setFill(Color.WHITE);
+
+        root.getChildren().addAll(start_text);
     }
 
 
